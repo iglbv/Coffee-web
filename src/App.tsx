50aa50b59d1d3d76c8ab732f445f589042bf778b
iglbv <ilyagolubev2021@gmail.com>
2024-11-12 20:55:38 +0500

    add Login and Register component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import CoffeeShop from './components/CoffeeShop/CoffeeShop';
 import AboutUs from './components/AboutUs';
 import Header from './components/Header';
@@ -7,19 +7,49 @@ import Footer from './components/Footer';
 import NotFound from './components/NotFound';
 import Contacts from './components/Contacts';
 import Feedback from './components/Feedback';
+import Login from './components/Login';
+import Register from './components/Register';
 
 const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    if (storedToken) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    if (storedToken && !isLoggedIn) {
+      setIsLoggedIn(true);
+    } else if (!storedToken && isLoggedIn) {
+      setIsLoggedIn(false);
+    }
+  }, [isLoggedIn]);
+
+  const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+    return isLoggedIn ? children : <Navigate to="/login" />;
+  };
+
+  const UnauthenticatedRoute = ({ children }: { children: React.ReactNode }) => {
+    return !isLoggedIn ? children : <Navigate to="/coffee-shop" />;
+  };
+
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-cream flex flex-col">
         <Header />
         <main className="flex-grow p-4">
           <Routes>
-            <Route path="/" element={<CoffeeShop />} />
-            <Route path="/coffee-shop" element={<CoffeeShop />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/about-us/contacts" element={<Contacts />} />
-            <Route path="/about-us/feedback" element={<Feedback />} />
+            <Route path="/" element={<ProtectedRoute><CoffeeShop /></ProtectedRoute>} />
+            <Route path="/coffee-shop" element={<ProtectedRoute><CoffeeShop /></ProtectedRoute>} />
+            <Route path="/about-us" element={<ProtectedRoute><AboutUs /></ProtectedRoute>} />
+            <Route path="/about-us/contacts" element={<ProtectedRoute><Contacts /></ProtectedRoute>} />
+            <Route path="/about-us/feedback" element={<ProtectedRoute><Feedback /></ProtectedRoute>} />
+            <Route path="/login" element={<UnauthenticatedRoute><Login setIsLoggedIn={setIsLoggedIn} /></UnauthenticatedRoute>} />
+            <Route path="/register" element={<UnauthenticatedRoute><Register /></UnauthenticatedRoute>} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,22 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
+    const username = localStorage.getItem('username');
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
+        window.location.reload();
+    };
+
     return (
         <header className="bg-primary text-light-text py-1 px-4 shadow-md">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to="/coffee-shop" className="text-2xl font-bold hover:text-accent">
                     <h1 className="text-2xl font-bold">Coffee Shop Radiovolna</h1>
                 </Link>
-                <nav className="flex items-center space-x-4">
-                    <Link to="/coffee-shop"
-                        className="text-gold hover:underline hover:text-accent">
-                        Кофейня
-                    </Link>
-                    <Link to="/about-us"
-                        className="text-gold hover:underline hover:text-accent">
-                        О нас
-                    </Link>
-                </nav>
+                {localStorage.getItem('token') ? (
+                    <nav className="flex items-center space-x-4">
+                        {username && (
+                            <span>
+                                Привет, {username}!
+                            </span>
+                        )}
+                        <Link to="/coffee-shop" className="hover:underline hover:text-accent">
+                            Кофейня
+                        </Link>
+                        <Link to="/about-us" className="hover:underline hover:text-accent">
+                            О нас
+                        </Link>
+                        <span onClick={handleLogout} className="hover:underline hover:text-accent cursor-pointer">
+                            Выйти
+                        </span>
+                    </nav>
+                ) : (
+                    <></>
+                )}
             </div>
         </header>
     );
